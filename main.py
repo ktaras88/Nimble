0b5b80eb2e2de39480e8ae4d6aed4c160886474c
TarasKhaburskyi <ktaras88@gmail.com>
2023-01-12 09:48:11 +0100

    change names

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ def create_or_update_file(key: str, file: UploadFile):
 
 @app.get("/{key}")
 def get_file(key: str):
-    if obj := download_file(s3(), key, AWS_S3_BUCKET_NAME):
-        return Response(content=obj['content'], media_type=obj['media_type'],
+    if download_obj := download_file(s3(), key, AWS_S3_BUCKET_NAME):
+        return Response(content=download_obj['content'], media_type=download_obj['media_type'],
                         status_code=status.HTTP_200_OK)
     else:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
